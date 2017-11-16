6f62447b29392fa6f7f839745bca76b9120ed850
Jace McPherson <jace-mcpherson@att.net>
2017-11-16 01:23:37 -0600

    Test commit.

--- src/main/java/io/lovelacetech/lovelaceassetmanager/LovelaceAssetManager.java
+++ src/main/java/io/lovelacetech/lovelaceassetmanager/LovelaceAssetManager.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LovelaceAssetManager {
+	// I'm adding this comment to test the Codeship auto-deploy
 	public static void main(String[] args) {
 		SpringApplication.run(LovelaceAssetManager.class, args);
 	}
