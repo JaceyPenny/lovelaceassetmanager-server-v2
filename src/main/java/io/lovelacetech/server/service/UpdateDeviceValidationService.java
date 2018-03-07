778e8f670acddbc551aa50072e2ecb9e21b0ea8b
Jace McPherson <jace-mcpherson@att.net>
2018-03-07 01:36:25 -0600

    Added log messages - debugging device

--- src/main/java/io/lovelacetech/server/service/UpdateDeviceValidationService.java
+++ src/main/java/io/lovelacetech/server/service/UpdateDeviceValidationService.java
@@ -22,12 +22,18 @@ public class UpdateDeviceValidationService {
 
     long currentEpochSecond = Calendar.getInstance().toInstant().getEpochSecond();
 
+    System.out.println("TIMESTAMP: " + timestamp);
+    System.out.println("SERVER TIMESTAMP: " + currentEpochSecond);
+
     if (Math.abs(currentEpochSecond - timestamp) > 300) { // check if timestamp is within 5 minutes
       return UpdateDeviceResponse.INVALID_TIMESTAMP;
     }
 
     String preHashString = url.replaceAll("/", "") + "/" + deviceCode + updateDeviceSecret + timestamp;
     String hashedString = HashUtil.getSha256HashString(preHashString);
+
+    System.out.println("PREHASH: " + preHashString);
+    System.out.println("HASH: " + hashedString);
 
     return hashedString.equalsIgnoreCase(hash) ?
         UpdateDeviceResponse.SUCCESS :
